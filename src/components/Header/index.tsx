2ca1ced6c2588653665c643653283345df31c850
mndiaz90 <mayler900123@gmail.com>
2021-05-04 14:30:53 -0300

    Corrige estilos do componente header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,6 @@ import styles from './styles.module.scss'
 export const Header = () => {
     return <div className={styles.container}>
         <img src="/Logo-SoftExpert.gif" alt="" />
-        <h3>Stock Exchange Challenge</h3>
+        <h2>Stock Exchange Challenge</h2>
     </div>
 }
