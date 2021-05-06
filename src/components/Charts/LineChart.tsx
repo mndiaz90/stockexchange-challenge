312a5f1d9c157892f4f497415230b3914ed14e0b
mndiaz90 <mayler900123@gmail.com>
2021-05-06 16:55:17 -0300

    Adiciona css para ficar responsive

--- src/components/Charts/LineChart.tsx
+++ src/components/Charts/LineChart.tsx
@@ -3,68 +3,65 @@ import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 
 type LineChartProps = {
-    title?: string;
-    pointStart: string;
-    series: number[]
+	title?: string;
+	pointStart: string;
+	series: number[]
 }
 export const LineChart = ({ title = 'Balanço geral', pointStart, series }: LineChartProps) => {
 
-    const options = {
-        chart: {
-            zoomType: 'xy'
-        },
-        title: {
-            text: title
-        },
-        subtitle: {
-            text: 'Demonstraçoes de resultados anuais'
-        },
-        yAxis: {
-            title: {
-                text: ''
-            },
-            labels: {
-                format: 'U$ {value}',
-            },
-        },
+	const options = {
+		chart: {
+			zoomType: 'xy'
+		},
+		title: {
+			text: title
+		},
+		subtitle: {
+			text: 'Demonstraçoes de resultados anuais'
+		},
+		yAxis: {
+			title: {
+				text: ''
+			},
+			labels: {
+				format: 'U$ {value}',
+			},
+		},
+		xAxis: {
+			accessibility: {
+				rangeDescription: 'Range: Last 10 years'
+			}
+		},
+		legend: {
+			layout: 'horizontal',
+			align: 'center',
+			verticalAlign: 'bottom'
+		},
+		plotOptions: {
+			series: {
+				label: {
+					connectorAllowed: false
+				},
+				pointStart: Number(pointStart)
+			}
+		},
+		series: series,
+		responsive: {
+			rules: [{
+				condition: {
+					maxWidth: 500
+				},
+				chartOptions: {
+					legend: {
+						layout: 'horizontal',
+						align: 'center',
+						verticalAlign: 'bottom'
+					}
+				}
+			}]
+		}
 
-        xAxis: {
-            accessibility: {
-                rangeDescription: 'Range: Last 10 years'
-            }
-        },
+	};
 
-        legend: {
-            layout: 'horizontal',
-            align: 'center',
-            verticalAlign: 'bottom'
-        },
-
-        plotOptions: {
-            series: {
-                label: {
-                    connectorAllowed: false
-                },
-                pointStart: Number(pointStart)
-            }
-        },
-        series: series,
-        responsive: {
-            rules: [{
-                condition: {
-                    maxWidth: 500
-                },
-                chartOptions: {
-                    legend: {
-                        layout: 'horizontal',
-                        align: 'center',
-                        verticalAlign: 'bottom'
-                    }
-                }
-            }]
-        }
-
-    };
-
-    return <HighchartsReact highcharts={Highcharts} options={options} />
+	return <HighchartsReact highcharts={Highcharts} options={options} />
 }
