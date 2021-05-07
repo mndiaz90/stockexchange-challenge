f46c567ae4ac3b558e04444ef267a706f99f8c33
mndiaz90 <mayler900123@gmail.com>
2021-05-07 14:58:45 -0300

    Troca valor da apikey

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,4 @@ export const api = axios.create({
     baseURL: 'https://financialmodelingprep.com/api/v3/'
 });
 
-export const apikey = '66b3224598c7c0a79b44b4fdac4a97dc';
+export const apikey = 'a2da85564c0c825286c8d8ee11574dc4';
